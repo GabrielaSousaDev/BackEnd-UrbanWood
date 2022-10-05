86c81e6befdc40892cf920eb4689b6d22e0e2c46
Gabriela Sousa <gabrielasousa.dev@gmail.com>
2022-10-05 19:09:40 -0300

    Update models

--- src/main/java/br/sc/senac/urbanwood/model/Address.java
+++ src/main/java/br/sc/senac/urbanwood/model/Address.java
@@ -8,9 +8,13 @@ import javax.persistence.Id;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity
 @Table(name="address")
 public class Address{
--- src/main/java/br/sc/senac/urbanwood/model/Furniture.java
+++ src/main/java/br/sc/senac/urbanwood/model/Furniture.java
@@ -2,10 +2,14 @@ package br.sc.senac.urbanwood.model;
 
 import java.util.List;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -15,16 +19,48 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Data
 @Entity
+@Table(name="furniture")
 public class Furniture {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name="id_furniture")
 	private Long id;
+	
+	@Column(name="name_furniture")
 	private String name;
+	
+	@Column(name="price_furniture")
 	private Double price;
-	private Image imagem;
+	
+	@Column(name="description_furniture")
 	private String description;
+	
+	@Column(name="furniture_size_furniture")
 	private Double furnitureSize;
+	
+	@OneToOne(mappedBy = "image")
+	private Image imagem;
+	
+	@OneToMany(mappedBy = "color")
 	private List<Color> color;
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	
 }
