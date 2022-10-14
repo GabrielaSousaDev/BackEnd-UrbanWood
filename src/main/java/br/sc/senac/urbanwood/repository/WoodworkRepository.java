6c44fe0f8d2b00b8b9053a64ae818815a8166f77
Gabriela Sousa <gabrielasousa.dev@gmail.com>
2022-10-14 15:31:57 -0300

    Update

--- src/main/java/br/sc/senac/urbanwood/repository/WoodworkRepository.java
+++ src/main/java/br/sc/senac/urbanwood/repository/WoodworkRepository.java
@@ -1,12 +1,21 @@
 package br.sc.senac.urbanwood.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import br.sc.senac.urbanwood.model.Woodwork;
+import br.sc.senac.urbanwood.projection.client.ClientProjection;
+import br.sc.senac.urbanwood.projection.woodwork.WoodworkProjection;
 
 @Repository
 public interface WoodworkRepository extends JpaRepository<Woodwork, Long> {
+	
+	@Query(value = "SELECT w.cnpj AS cnpj, w.companyName AS companyName, w.description AS description FROM woodwork w")
+	List<WoodworkProjection> findWoodworks();
+
 /*
 	boolean existsById(Long id);
 
--- src/main/java/br/sc/senac/urbanwood/service/woodwork/WoodworkService.java
+++ src/main/java/br/sc/senac/urbanwood/service/woodwork/WoodworkService.java
@@ -1,29 +1,41 @@
 package br.sc.senac.urbanwood.service.woodwork;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
+import br.sc.senac.urbanwood.dto.WoodworkDTO;
 import br.sc.senac.urbanwood.dto.woodwork.AllWoodworkDTO;
+import br.sc.senac.urbanwood.projection.woodwork.WoodworkProjection;
 
 @Service
 public interface WoodworkService {
 
-    AllWoodworkDTO save(AllWoodworkDTO dto);
+	AllWoodworkDTO save(AllWoodworkDTO dto);
 
-    void delete(Long id);
-/*    void update(WoodworkDTO woodworkDTO, Long id);
+	void delete(Long id);
 
-   
+	void update(WoodworkDTO woodworkDTO, Long id);
 
-    ProfileWoodworkFullEditProjection findById(Long id); 
+	AllWoodworkDTO findById(Long id);
+	
+	List<WoodworkProjection> findAll();
 
-    List<ProfileWoodworkForEditProjection> findByCompanyName(String companyName);
-    
-    ProfileWoodworkForEditProjection findByCnpj(String cnpj);
-
-    List<ProfileWoodworkForEditProjection> findByPhoneNumber(String phoneNumber);
-    
-    List<ProfileWoodworkForEditProjection> findByNeighborhood(String neighborhood);
-
-	*/
+	/*
+	 * 
+	 * 
+	 * 
+	 * ProfileWoodworkFullEditProjection findById(Long id);
+	 * 
+	 * List<ProfileWoodworkForEditProjection> findByCompanyName(String companyName);
+	 * 
+	 * ProfileWoodworkForEditProjection findByCnpj(String cnpj);
+	 * 
+	 * List<ProfileWoodworkForEditProjection> findByPhoneNumber(String phoneNumber);
+	 * 
+	 * List<ProfileWoodworkForEditProjection> findByNeighborhood(String
+	 * neighborhood);
+	 * 
+	 */
 
 }
