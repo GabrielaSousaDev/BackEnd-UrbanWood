deca9d959ef7101ff504d3b3f3358437a78469d0
Gabriela Sousa <gabrielasousa.dev@gmail.com>
2022-10-14 17:15:42 -0300

    Update

--- src/main/java/br/sc/senac/urbanwood/repository/EnvironmentRepository.java
+++ src/main/java/br/sc/senac/urbanwood/repository/EnvironmentRepository.java
@@ -1,26 +1,20 @@
 package br.sc.senac.urbanwood.repository;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import br.sc.senac.urbanwood.dto.EnvironmentDTO;
+import br.sc.senac.urbanwood.dto.environment.AllEnvironmentDTO;
 import br.sc.senac.urbanwood.model.Environment;
-import br.sc.senac.urbanwood.projection.EnvironmentProjection;
 
 @Repository
 public interface EnvironmentRepository extends JpaRepository<Environment, Long> {
-/*
 
-	EnvironmentDTO save(EnvironmentDTO livingAreaDTO);
 
-	void update(EnvironmentDTO environmentDTO, Long id);
+	AllEnvironmentDTO save(AllEnvironmentDTO dto);
+
+	void update(AllEnvironmentDTO AllEnvironmentDTO, Long id);
 
 	void delete(Long id);
 
-	Environment save(Environment environment1);
 
-	List<EnvironmentProjection> listByWoodworkById(Long id);
-*/
 }
--- src/main/java/br/sc/senac/urbanwood/service/environment/EnvironmentServiceImpl.java
+++ src/main/java/br/sc/senac/urbanwood/service/environment/EnvironmentServiceImpl.java
@@ -2,10 +2,16 @@ package br.sc.senac.urbanwood.service.environment;
 
 import org.springframework.stereotype.Service;
 
+import br.sc.senac.urbanwood.dto.environment.AllEnvironmentDTO;
+import br.sc.senac.urbanwood.exception.environment.EnvironmentNotFoundException;
+import br.sc.senac.urbanwood.mapper.EnvironmentMapper;
+import br.sc.senac.urbanwood.model.Environment;
+import br.sc.senac.urbanwood.repository.EnvironmentRepository;
+
 
 @Service
 public class EnvironmentServiceImpl implements EnvironmentService {
-/*
+
 	private final EnvironmentMapper environmentMapper;
 	private final EnvironmentRepository environmentRepository;
 
@@ -14,29 +20,39 @@ public class EnvironmentServiceImpl implements EnvironmentService {
 		this.environmentMapper = environmentMapper;
 	}
 
-	public EnvironmentDTO save(EnvironmentDTO environmentDTO) {
+	public AllEnvironmentDTO save(AllEnvironmentDTO dto) {
 
-		Environment environment = environmentMapper.toEntity(environmentDTO);
+		Environment environment = new Environment(dto.idEnvironment(), dto.name(), null, null, null);
 		Environment environmentSaved = environmentRepository.save(environment);
+		
 		return environmentMapper.toDTO(environmentSaved);
 	}
 
-	public void update(EnvironmentDTO environmentDTO, Long id) {
+	public void update(AllEnvironmentDTO dto, Long id) {
 
 		Environment environment = (Environment) environmentRepository.findById(id)
 				.orElseThrow(() -> new EnvironmentNotFoundException("Environment " + id + " was not found"));
 
-		environment.setName(environmentDTO.name());
+		environment.setName(dto.name());
 		environmentRepository.save(environment);
 	}
 
 	public void delete(Long id) {
-		if (!EnvironmentRepository.existsById(id))
+		
+		if (!environmentRepository.existsById(id))
 			throw new EnvironmentNotFoundException("Environment " + id + " was not found");
+		
 		environmentRepository.delete(id);
 	}
 
-	public EnvironmentProjection findById(Long id) {
+	
+
+	@Override
+	public boolean existsById(Long id) {
+		return false;
+	}
+
+	/*public EnvironmentProjection findById(Long id) {
 		return environmentRepository.findById(id)
 				.orElseThrow(() -> new EnvironmentNotFoundException("Environment " + id + " was not found"));
 
